bc98c06cde467510b0cd31869b68f2b40ab79f60
czhoume <czhoume@gmail.com>
2016-01-10 01:41:32 -0800

    add button functions

--- showrecords.php
+++ showrecords.php
@@ -18,12 +18,31 @@ $time= new DateTime();
 $time->sub(new DateInterval('P'.$_GET['days'].'D'));
 $time=$time->format('Y-m-d H:i:s');
 // $time=date('Y-m-d H:i:s', time());
-$sql = "SELECT Time, description FROM pprecords LEFT JOIN ppcategory on pprecords.descriptionid= ppcategory.descriptionid WHERE Time > '$time'";
+$sql = "SELECT Time, description, descriptionid FROM pprecords LEFT JOIN ppcategory on pprecords.descriptionid= ppcategory.descriptionid WHERE Time > '$time'";
 $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
 	echo "<table border=\"1\"><tr><th>Time</th><th>Activity</th></tr>";
     while($row = mysqli_fetch_assoc($result)) {
-        echo "<tr><td>" . $row["Time"]. "</td><td>" . $row["description"]. "</td></tr>";
+    	switch ($row["descriptionid"]) {
+    		case 10:
+    		case 11:
+        		$bgc="red";
+        		break;
+	    	case 8:
+	    	case 9:
+	        	$bgc="orangered";
+	        	break;
+	    	case 7:
+	    	case 6:
+	        	$bgc="yellow";
+	        	break;
+	        case 5:
+	        case 4:
+	        	$bgc="lightgreen"
+	    	default:
+        		echo "white";
+}
+        echo "<tr bgcolor=".$bgc."><td>" . $row["Time"]. "</td><td>" . $row["description"]. "</td></tr>";
     }
 } else {
     echo "invalid activity";
