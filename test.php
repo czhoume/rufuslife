ee6145bc863d7a0c0ebe58c1795a6e2283a5f31f
czhoume <czhoume@gmail.com>
2016-01-10 00:41:37 -0800

    add button functions

--- test.php
+++ test.php
@@ -4,5 +4,6 @@ $time= new DateTime();
 echo $time->format('Y-m-d H:i:s') . "\n";
 $time->sub(new DateInterval('PT'.$num.'M'));
 echo $time->format('Y-m-d H:i:s') . "\n"; 
-$time=$time->format('Y-m-d H:i:s'); 
+$time=$time->format('Y-m-d H:i:s');
+echo $time; 
 ?>
