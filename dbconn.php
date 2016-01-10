b932ced29e25b0ce1cc9b2ed290e97923ea68738
czhoume <czhoume@gmail.com>
2016-01-09 19:51:18 -0800

    try dbconn

--- dbconn.php
+++ dbconn.php
@@ -11,4 +11,17 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 echo "Connected successfully";
+
+$time= date('m/d/Y h:i:s a', time());
+$id=1;
+
+$sql = "INSERT INTO pprecords (Time, descriptionid, delta)
+VALUES ('$time', '$id', '0')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
 ?>
