6c1d4374bc9fbfd069248d070e6b972ebf8654fd
czhoume <czhoume@gmail.com>
2016-01-10 00:46:51 -0800

    add button functions

--- test.php
+++ test.php
@@ -1,9 +1,12 @@
 <?php 
-$num=10;
+$id=$_POST['act'];
+echo $id."<br>";
+$num=$_POST['delay'];
+echo $num."<br>";
 $time= new DateTime();
-echo $time->format('Y-m-d H:i:s') . "\n";
+echo $time->format('Y-m-d H:i:s') . "<br>";
 $time->sub(new DateInterval('PT'.$num.'M'));
-echo $time->format('Y-m-d H:i:s') . "\n"; 
+echo $time->format('Y-m-d H:i:s') . "<br>"; 
 $time=$time->format('Y-m-d H:i:s');
 echo $time; 
 ?>
