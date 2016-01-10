2b42db316456c90f6fbf9ea1e143d3929587a2db
czhoume <czhoume@gmail.com>
2016-01-09 23:52:02 -0800

    add button functions

--- dbconn.php
+++ dbconn.php
@@ -21,6 +21,7 @@ $time= date('Y-m-d h:i:s', time());
 $sql = "SELECT description FROM ppcategory WHERE descriptionid = $id";
 echo $sql;
 $result = mysqli_query($conn, $sql);
+echo "yes?";
 if ($mysqli_num_rows($result) > 0) {
     $row = mysqli_fetch_assoc($result);
     echo $row["description"];
