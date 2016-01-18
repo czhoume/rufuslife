d807f227a2b0e0ffb6ea4837e12e0a8841036237
czhoume <czhoume@gmail.com>
2016-01-17 23:57:20 -0800

    add button color

--- calculate.php
+++ calculate.php
@@ -18,8 +18,8 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
 	echo "<table border=\"1\"><tr><th>Time</th><th>Activity</th></tr>";
     while($row = mysqli_fetch_assoc($result)) {
-    	$tab[$count]["Time"]=$row["Time"];
-    	$tab[$count]["description"]=$row["description"];
+    	$tab[$count][0]=$row["Time"];
+    	$tab[$count][1]=$row["description"];
     	$count++;
     }
 } else {
@@ -29,33 +29,33 @@ if (mysqli_num_rows($result) > 0) {
 
 
 for($i=0; $i<$count; $i++){
-	if(strpos($tab[$i]["description"],'drink') !== false){
-		$drink=$tab[$i]["Time"];
-	}elseif(strpos($tab[$i]["description"],'feed') !== false){
-		$feed=$tab[$i]["Time"];
-	}elseif(strpos($tab[$i]["description"],'pee') !== false){
+	if(strpos($tab[$i][1],'drink') !== false){
+		$drink=$tab[$i][0];
+	}elseif(strpos($tab[$i][1],'feed') !== false){
+		$feed=$tab[$i][0];
+	}elseif(strpos($tab[$i][1],'pee') !== false){
 		if($lastpee==null)
-			$lastpee=$tab[$i]["Time"];
+			$lastpee=$tab[$i][0];
 		else{
-			$tab[$i]["delta"]=round(abs($strtotime($tab[$i]["Time"]) - $strtotime($lastpee)) / 60,2);
+			$tab[$i][2]=round(abs($strtotime($tab[$i][0]) - $strtotime($lastpee)) / 60,2);
 			if($drink!=null)
-				$tab[$i]["deltatoactivity"]=round(abs($strtotime($tab[$i]["Time"]) - $strtotime($drink)) / 60,2);
-			$lastpee=$tab[$i]["Time"];
+				$tab[$i][3]=round(abs($strtotime($tab[$i][0]) - $strtotime($drink)) / 60,2);
+			$lastpee=$tab[$i][0];
 		}
-	}elseif(strpos($tab[$i]["description"],'poop') !== false){
+	}elseif(strpos($tab[$i][1],'poop') !== false){
 		if($lastpoop==null)
-			$lastpoop=$tab[$i]["Time"];
+			$lastpoop=$tab[$i][0];
 		else{
-			$tab[$i]["delta"]=round(abs($strtotime($tab[$i]["Time"]) - $strtotime($lastpoop)) / 60,2);
+			$tab[$i][2]=round(abs($strtotime($tab[$i][0]) - $strtotime($lastpoop)) / 60,2);
 			if($feed!=null)
-				$tab[$i]["deltatoactivity"]=round(abs($strtotime($tab[$i]["Time"]) - $strtotime($feed)) / 60,2);
-			$lastpoop=$tab[$i]["Time"];
+				$tab[$i][3]=round(abs($strtotime($tab[$i][0]) - $strtotime($feed)) / 60,2);
+			$lastpoop=$tab[$i][0];
 	}
 }
 
 echo "<table border=\"1\"><tr><th>Time</th><th>Activity</th><th>Delta</th><th>Deltatoactivity</th></tr>";
 for($i=0; $i<$count; $i++){
-    	switch ($tab[$i]["description"]) {
+    	switch ($tab[$i][1]) {
     		case "poop outside cube indoor":
     			$bgc="red";
     			break;
@@ -81,7 +81,7 @@ for($i=0; $i<$count; $i++){
 	    	default:
 	        	$bgc="white";
 		}
-        echo "<tr bgcolor=".$bgc."><td>" . $tab[$i]["Time"]. "</td><td>" . $tab[$i]["description"]. "</td><td>" . $tab[$i]["delta"]. "</td><td>" . $tab[$i]["deltatoactivity"]. "</td></tr>";
+        echo "<tr bgcolor=".$bgc."><td>" . $tab[$i][0]. "</td><td>" . $tab[$i][1]. "</td><td>" . $tab[$i][2]. "</td><td>" . $tab[$i][3]. "</td></tr>";
     }
 mysqli_close($conn);
 
