1537310520ba4b2eb453ce81032311d27fee8cb3
czhoume <czhoume@gmail.com>
2016-01-17 23:16:09 -0800

    add button color

--- test.php
+++ test.php
@@ -5,6 +5,6 @@ $timenow=$time;
 echo $timenow->format('Y-m-d H:i:s') . "<br>";
 $time->sub(new DateInterval('PT10M'));
 echo $time->format('Y-m-d H:i:s') . "<br>";
-$diff=$timenow-$time;
+$diff=$timenow->diff($time);
 echo "look at this ". $diff;
 ?>
