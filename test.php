b0fe12f20f1e27a38e734066f3f3eca84b175600
czhoume <czhoume@gmail.com>
2016-01-17 23:24:58 -0800

    add button color

--- test.php
+++ test.php
@@ -7,4 +7,8 @@ $time= new DateTime("2016-1-17 11:23:00");
 echo $time->format('Y-m-d H:i:s') . "<br>";
 $diff=round ((($timenow - $time))/60);
 echo "look at this ". $diff;
+
+$to_time = strtotime("2008-12-13 10:42:00");
+$from_time = strtotime("2008-12-13 10:21:00");
+echo round(abs($to_time - $from_time) / 60,2). " minute";
 ?>
