1a48dd8ab368e5220b10f333eb127b728ce7770d
czhoume <czhoume@gmail.com>
2016-01-17 23:49:07 -0800

    add button color

--- calculate.php
+++ calculate.php
@@ -26,6 +26,63 @@ if (mysqli_num_rows($result) > 0) {
     echo "invalid activity";
 }
 
+
+
+for($i=0; i<count; i++){
+	if(strpos($tab[i]["description"],'drink') !== false){
+		$drink=$tab[i]["Time"];
+	}elseif(strpos($tab[i]["description"],'feed') !== false){
+		$feed=$tab[i]["Time"];
+	}elseif(strpos($tab[i]["description"],'pee') !== false){
+		if($lastpee==null)
+			$lastpee=$tab[i]["Time"];
+		else{
+			$tab[i]["delta"]=round(abs($strtotime($tab[i]["Time"]) - $strtotime($lastpee)) / 60,2)
+			if($drink!=null)
+				$tab[i]["deltatoactivity"]=round(abs($strtotime($tab[i]["Time"]) - $strtotime($drink)) / 60,2)
+			$lastpee=$tab[i]["Time"];
+		}
+	}elseif(strpos($tab[i]["description"],'poop') !== false){
+		if($lastpoop==null)
+			$lastpoop=$tab[i]["Time"];
+		else{
+			$tab[i]["delta"]=round(abs($strtotime($tab[i]["Time"]) - $strtotime($lastpoop)) / 60,2)
+			if($feed!=null)
+				$tab[i]["deltatoactivity"]=round(abs($strtotime($tab[i]["Time"]) - $strtotime($feed)) / 60,2)
+			$lastpoop=$tab[i]["Time"];
+	}
+}
+
+echo "<table border=\"1\"><tr><th>Time</th><th>Activity</th><th>Delta</th><th>Deltatoactivity</th></tr>";
+for($i=0; i<count; i++){
+    	switch ($tab[i]["description"]) {
+    		case "poop outside cube indoor":
+    			$bgc="red";
+    			break;
+    		case "pee outside cube indoor":
+        		$bgc="red";
+        		break;
+	    	case "pee inside cube not on pad":
+	    	case "poop inside cube not on pad":
+	        	$bgc="orangered";
+	        	break;
+	    	case "poop inside cube on pad":
+	    		$bgc="#ffcc00";
+	    		break;
+	    	case "pee inside cube on pad":
+	        	$bgc="#ffff00";
+	        	break;
+	        case "poop outdoor":
+	        	$bgc="#009933";
+	        	break;
+	        case "pee outdoor":
+	        	$bgc="#33cc33";
+	        	break;
+	    	default:
+	        	$bgc="white";
+		}
+        echo "<tr bgcolor=".$bgc."><td>" . $row["Time"]. "</td><td>" . $row["description"]. "</td><td>" . $row["delta"]. "</td><td>" . $row["deltatoactivity"]. "</td></tr>";
+    }
 mysqli_close($conn);
 
 ?>
